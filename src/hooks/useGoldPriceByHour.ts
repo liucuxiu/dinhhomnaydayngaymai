ee15228bfac9d7b5aab3b3946807ee426bb42c19
liucuxiu <xliucuxiux@gmail.com>
2024-01-15 00:14:02 +0700

    call api

--- src/hooks/useGoldPriceByHour.ts
+++ src/hooks/useGoldPriceByHour.ts
@@ -2,7 +2,7 @@ import { useQuery } from 'react-query';
 
 const useGoldPriceByHour = () => {
   const fetchGoldPriceByHour = async () => {
-    const response = await fetch(import.meta.env.API);
+    const response = await fetch(import.meta.env.VITE_API);
     const data = await response.json();
     return data;
   };
