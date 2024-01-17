b74e70d439491a652ea8f286ce0c946467613247
liucuxiu <xliucuxiux@gmail.com>
2024-01-17 15:18:48 +0700

    Revert "change position"
    This reverts commit 35d0a2aa4876aadb9091ff97b161d4bd45a2de86.

--- src/components/PriceChart.tsx
+++ src/components/PriceChart.tsx
@@ -7,44 +7,42 @@ interface ChartProps {
 }
 
 const PriceChart: React.FC<ChartProps> = ({ data }) => (
-  <LineChart width={1000} height={500} data={data}>
-    <CartesianGrid strokeDasharray="3 3"/>
-    <XAxis dataKey="updatedAt"
-           tickFormatter={(date) => formatEpochDate(date)}/>
-    <YAxis type="number"
-           domain={['dataMin', 'dataMax']}
-           tickCount={8}
-           tickFormatter={(value) => `${value.toLocaleString()}`}
-    />
-    <Tooltip labelFormatter={(date) => formatEpochDate(date)}
-             formatter={(value) => `${value.toLocaleString()}`}
-    />
-    <Legend/>
+    <LineChart width={1000} height={500} data={data}>
+      <CartesianGrid strokeDasharray="3 3"/>
+      <XAxis dataKey="updatedAt"
+             tickFormatter={(date) => formatEpochDate(date)}/>
+      <YAxis  type="number" 
+              domain={['dataMin', 'dataMax']}
+              tickCount={8} 
+              tickFormatter={(value) => `${value.toLocaleString()}`}
+      />
+      <Tooltip labelFormatter={(date) => formatEpochDate(date)}
+               formatter={(value) => `${value.toLocaleString()}`}
+      />
+      <Legend/>
+      <Line type="monotone"
+            dataKey="nhanTronVRTL.sell"
+            name="Nhẫn tròn trơn VRTL (bán)" stroke="#fdbb18"
+            activeDot={{ r: 8 }} strokeWidth={3}/>
+      <Line type="monotone"
+            dataKey="nhanTronVRTL.buy"
+            name="Nhẫn tròn trơn VRTL (mua)"
+            stroke="#ecd089"
+            activeDot={{ r: 8 }} strokeWidth={3}/>
 
-    <Line type="monotone"
-          dataKey="vangMiengSJC.sell"
-          name="Vàng miếng SJC (bán)"
-          stroke="#cd1b1b"
-          activeDot={{ r: 8 }}
-          strokeWidth={3}/>
-    <Line type="monotone"
-          dataKey="vangMiengSJC.buy"
-          name="Vàng miếng SJC (mua)"
-          stroke="#f1a6a6"
-          activeDot={{ r: 8 }}
-          strokeWidth={3}/>
-    <Line type="monotone"
-          dataKey="nhanTronVRTL.sell"
-          name="Nhẫn tròn trơn VRTL (bán)"
-          stroke="#fdbb18"
-          activeDot={{ r: 8 }} strokeWidth={3}/>
-    <Line type="monotone"
-          dataKey="nhanTronVRTL.buy"
-          name="Nhẫn tròn trơn VRTL (mua)"
-          stroke="#ecd089"
-          activeDot={{ r: 8 }} strokeWidth={3}/>
-
-  </LineChart>
+      <Line type="monotone"
+            dataKey="vangMiengSJC.sell"
+            name="Nhẫn vàng miếng SJC (bán)"
+            stroke="#cd1b1b"
+            activeDot={{ r: 8 }}
+            strokeWidth={3}/>
+      <Line type="monotone"
+            dataKey="vangMiengSJC.buy"
+            name="Nhẫn vàng miếng SJC (mua)"
+            stroke="#f1a6a6"
+            activeDot={{ r: 8 }}
+            strokeWidth={3}/>
+    </LineChart>
 );
 
 export default PriceChart;
