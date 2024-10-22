41dc62e01d3cb3a05aaf9b2738e057917550b0bc
liucuxiu <xliucuxiux@gmail.com>
2024-10-22 13:58:11 +0700

    add analysis

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Analytics } from "@vercel/analytics/react"
 
 import { QueryClient, QueryClientProvider } from 'react-query'
 
@@ -21,7 +21,8 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-    <App />
+      <Analytics/>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>,
 )
